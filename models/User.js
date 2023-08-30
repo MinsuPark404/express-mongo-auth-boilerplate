270f9a678995a944d3c0d54a307d26b8fd2e7a7a
PMS <parkms6490@gmail.com>
2023-08-30 21:38:53 +0900

    test

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 const userSchema = mongoose.Schema({
   user_name: {
     type: String,
@@ -30,6 +34,43 @@ const userSchema = mongoose.Schema({
   }
 })
 
+userSchema.pre('save', (next)=>{
+  var user = this;
+
+  if(user.isModified('user_password')){
+    // 비밀번호를 암호화 시킵니다.
+    bcrypt.genSalt(saltRounds, (err, salt)=>{
+      if(err) return next(err);
+      bcrypt.hash(myPlaintextPassword, salt, (err, hash)=>{
+        // myPlaintextPassword : 암호화 전 비밀번호
+        // hash : 암호화된 비밀번호
+        // 비밀번호 DB에 고정된 길이의 문자열로 변환 후 저장합니다.
+        if(err) return next(err);
+        user.password = hash
+        next()
+      });
+    });
+  }
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = {User}
+
+
+
+
+
+
+    // 비밀번호를 암호화 시킵니다.
+    // bcrypt.genSalt(saltRounds, (err, salt)=>{
+    //   if(err) return next(err);
+    //   bcrypt.hash(myPlaintextPassword, salt, (err, hash)=>{
+    //     // myPlaintextPassword : 암호화 전 비밀번호
+    //     // hash : 암호화된 비밀번호
+    //     // 비밀번호 DB에 고정된 길이의 문자열로 변환 후 저장합니다.
+    //     if(err) return next(err);
+    //     user.password = hash
+    //     next()
+    //   });
+    // });
