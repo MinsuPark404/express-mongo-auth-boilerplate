b16b0f5f2648b667e8fda8ba74dabefcadc30838
Minsu Park <parkms6490@gmail.com>
2023-09-01 01:25:40 +0900

    login

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = mongoose.Schema({
@@ -8,7 +9,7 @@ const userSchema = mongoose.Schema({
     type: String,
     maxlength: 50
   },
-  email: {
+  user_email: {
     type: String,
     trim: true,
     unique: 1,
@@ -56,6 +57,22 @@ userSchema.methods.comparePassword = function(plainPassword, cb){
   bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
     if(err) return cb(err),
       cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb){
+  var user = this;
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sige(user._id.toHexString(), 'secretToken')
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
   })
 }
 
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { User } = require("./models/User");
-
+const cookieParser = require('cookie-parser');
 const config = require("./config/key")
+
+const { User } = require("./models/User");
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 
@@ -36,19 +39,31 @@ app.post("/register", async (req, res) => {
 
 app.post('/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ user_email: req.body.emil }, (err, userInfo) => {
+  User.findOne({ user_email: req.body.user_emil }, (err, userInfo) => {
     if(!userInfo) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다."
       })
     }
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+
+    user.comparePassword(req.body.user_password, (err, isMatch) => {
+      if(!isMatch) 
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+      
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
+      })      
+    })    
   })
-  // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
-  user.comparePassword(req.body.user_password, (err, isMatch) => {
-    if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-  })
-  // 비밀번호까지 맞다면 토큰을 생성하기.
 })
 
 app.listen(port, () => {
